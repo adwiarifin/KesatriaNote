e07a76995955998ab9d6de1c928e8a98772d16eb
Adwi Arifin <adwiarifin@gmail.com>
2018-06-26 23:00:35 +0700

    move MainActivity to view package

--- app/src/main/java/com/kesatriakeyboard/kesatrianote/view/MainActivity.java
+++ app/src/main/java/com/kesatriakeyboard/kesatrianote/view/MainActivity.java
@@ -1,7 +1,9 @@
-package com.kesatriakeyboard.kesatrianote;
+package com.kesatriakeyboard.kesatrianote.view;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.kesatriakeyboard.kesatrianote.R;
 
 public class MainActivity extends AppCompatActivity {
 
